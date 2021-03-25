af78a21dae4e69ce81f00b4bbe870808f4e14106
KalleshMarad <43565738+KalleshMarad@users.noreply.github.com>
2021-03-25 21:47:10 +0530

    added the scripting groovy file

---
+++ script.groovy
@@ -0,0 +1,10 @@
+def buildApp(){
+  echo 'building the application 2nd line'
+}
+def testApp(){
+  echo 'testing the application 2nd line'
+}
+def deployApp(){
+  echo 'deploying the application 2nd line'
+}
+return this
